71d5827033e95c1bb51bec9d146fce1291cdbf84
Ronaldo Johnson <Ronaldo.Johnson@mutualofomaha.com>
2021-02-05 15:47:50 -0600

    initial commit

--- README.md
+++ README.md
@@ -6,9 +6,19 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+# Needed packages
+
+@aws-cdk/aws-s3 @aws-cdk/aws-lambda @aws-cdk/aws-lambda-event-sources @aws-cdk/aws-dynamodb @aws-cdk/iam
+
+# if issues with cli compatibility when cdk synth
+
+npm install -g aws-cdk@latest
+or
+yarn global upgrade aws-cdk
--- lib/devhour-stack.ts
+++ lib/devhour-stack.ts
@@ -1,9 +1,49 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
+import iam = require('@aws-cdk/aws-iam');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import { Duration } from '@aws-cdk/core';
+
+const imageBucketName = "cdk-rekn-imagebucket";
 
 export class DevhourStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const imageBucket = new s3.Bucket(this, imageBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
+
+    const table = new dynamodb.Table(this, 'ImageLabels', {
+      partitionKey: { name: 'image', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    new cdk.CfnOutput(this, 'ddTable', { value: table.tableName });
+
+    const rekFn = new lambda.Function(this, 'rekognitionFuntion', {
+      code: lambda.Code.fromAsset('rekognitionlambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      memorySize: 1024,
+      environment: {
+        "TABLE": table.tableName,
+        "BUCKET": imageBucket.bucketName
+      }
+    });
+    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [s3.EventType.OBJECT_CREATED] }));
+    imageBucket.grantRead(rekFn);
+    table.grantWriteData(rekFn);
+
+    rekFn.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['rekognition:DetectLabels'],
+      resources: ['*']
+
+    }));
   }
+
 }
